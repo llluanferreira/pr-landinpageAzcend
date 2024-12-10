6c78fda4b06b9f6c51e800ca62d20e356eb568a3
llluanferreira <luanfrodrigues70@gmail.com>
2024-12-10 10:18:55 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Azcend</title>
+    <link rel="icon" href="https://azcend.com.br/wp-content/uploads/2023/09/cropped-icone-32x32.png" sizes="32x32" />
+    <link rel="icon" href="https://azcend.com.br/wp-content/uploads/2023/09/cropped-icone-192x192.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://azcend.com.br/wp-content/uploads/2023/09/cropped-icone-180x180.png" />
+    <meta name="msapplication-TileImage" content="https://azcend.com.br/wp-content/uploads/2023/09/cropped-icone-270x270.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+
+</head>
+
+<body class="bg-dark text-white">
+    <section>
+        <nav class="navbar navbar-expand-lg navbar-dark px-4">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="/assets/img/logoazcend].png" alt="Azcend Logo">
+                </a>
+            </div>
+        </nav>
+    </section>
+    <section>
+        <header class="d-flex align-items-center top60">
+            <div class="container">
+                <div class="row align-items-center">
+
+                    <div class="col-md-6 blocoinicial">
+                        <h2 class="textoinicial">Transforme seus resultados no <br> e-commerce agora mesmo: escolha o gateway<span class="text-primary"> Azcend!</span>.</h2>
+                        <h3 class="lead">A evolução dos pagamentos online: Gateway e checkout de alta performance para dropshipping e infoprodutos.</h3>
+                        <a href="https://accounts.azcend.com.br/signup" target="_blank" class="btn nav-link text-white btn-navbarbuttom">Criar uma conta</a>
+                        <a href="https://azcend.com.br/" target="_blank" class="btn nav-link text-white btn-navbarbuttom">Acessar nosso site</a>
+                        <a href="https://api.whatsapp.com/send/?phone=5527996933863&text&type=phone_number&app_absent=0"  target="_blank" class="btn nav-link text-white btn-navbarbuttom">Whatsapp - Time Comercial</a>
+                    </div>
+
+                    <div class="col-md-6 text-center position-relative">
+
+                        <picture class="spinner">
+                            <source srcset="https://azcend.com.br/wp-content/webp-express/webp-images/uploads/2023/09/2.png.webp" type="image/webp">
+                            <img decoding="async" width="170" height="160" src="/assets/img/imagemdosite.png" class="attachment-full size-full wp-image-100 webpexpress-processed" alt="">
+                        </picture>
+                        <img src="/assets/img/imagemdosite.png" alt="Imagem de Destaque" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </header>
+    </section>
+        <footer class="rodape mt-5">
+            <div class="container text-center">
+                <img src="https://azcend.com.br/wp-content/webp-express/webp-images/uploads/2023/09/44-1-1024x320.png.webp" alt="Logo Azcend" class="logo-rodape">
+                <div class="social-icons mt-3">
+                    <a href="#" target="_blank"><img src="https://img.icons8.com/win10/512/FFFFFF/instagram-new.png" alt="Instagram" class="icone-social"></a>
+                    <a href="#" target="_blank"><img src="https://img.icons8.com/ios_filled/512/FFFFFF/youtube-play.png" alt="YouTube" class="icone-social"></a>
+                </div>
+                <p class="info-rodape mt-3">
+                    Azcend Plataforma de Pagamentos | 50.077.382/0001-20 <br>
+                    2023 - Todos os Direitos Reservados <br>
+                    E-mail: suporte@azcend.com.br
+                </p>
+            </div>
+        </footer>
+
+    </section>
+
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</html>
